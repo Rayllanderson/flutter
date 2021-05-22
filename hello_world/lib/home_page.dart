c59c7a03afb067a1b22816eb84bd239de55e3ed1
Rayllanderson <rayllanderson@gmail.com>
2021-05-22 15:10:37 -0300

    Componentizando o switch e colocando na navbar

--- hello_world/lib/home_page.dart
+++ hello_world/lib/home_page.dart
@@ -29,15 +29,11 @@ class HomePageState extends State<HomePage> {
           return Scaffold(
             appBar: AppBar(
               title: Text('Home Page'),
+              actions: [CustomSwitch()],
             ),
             body: Center(
                 child: GestureDetector(
-              child: Switch(
-                value: appController.isDarkTheme,
-                onChanged: (value) {
-                  appController.changeTheme();
-                },
-              ),
+              child: CustomSwitch(),
             )),
             floatingActionButton: FloatingActionButton(
               child: Icon(Icons.add),
@@ -47,5 +43,19 @@ class HomePageState extends State<HomePage> {
             ),
           );
         });
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  const CustomSwitch({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: AppController.instance.isDarkTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
+    );
   }
 }
