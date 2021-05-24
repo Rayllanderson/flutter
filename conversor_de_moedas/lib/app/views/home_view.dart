88d511899dc24712d42ef8e98b30dc7252f3562a
Rayllanderson <rayllanderson@gmail.com>
2021-05-24 15:30:23 -0300

    Fix scrollbar

--- conversor_de_moedas/lib/app/views/home_view.dart
+++ conversor_de_moedas/lib/app/views/home_view.dart
@@ -22,53 +22,55 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: EdgeInsets.only(top: 100, left: 30, right: 30),
-          child: Column(
-            children: [
-              Image.asset('assets/images/logo.png', width: 150, height: 150),
-              SizedBox(
-                height: 50,
-              ),
-              CurrencyBox(
-                textEdit: toText,
-                selectedItem: homeController.toCurrency,
-                items: homeController.currencies,
-                onChanged: (model) {
-                  setState(() {
-                    homeController.toCurrency = model;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CurrencyBox(
-                textEdit: fromText,
-                selectedItem: homeController.fromCurrency,
-                items: homeController.currencies,
-                onChanged: (model) {
-                  setState(() {
-                    homeController.fromCurrency = model;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.amber,
-                  ),
-                  onPressed: () {
-                    homeController.convert();
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: EdgeInsets.only(top: 100, left: 30, right: 30),
+            child: Column(
+              children: [
+                Image.asset('assets/images/logo.png', width: 150, height: 150),
+                SizedBox(
+                  height: 50,
+                ),
+                CurrencyBox(
+                  textEdit: toText,
+                  selectedItem: homeController.toCurrency,
+                  items: homeController.currencies,
+                  onChanged: (model) {
+                    setState(() {
+                      homeController.toCurrency = model;
+                    });
                   },
-                  child:
-                      Text('CONVERTER', style: TextStyle(color: Colors.black)))
-            ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CurrencyBox(
+                  textEdit: fromText,
+                  selectedItem: homeController.fromCurrency,
+                  items: homeController.currencies,
+                  onChanged: (model) {
+                    setState(() {
+                      homeController.fromCurrency = model;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.amber,
+                    ),
+                    onPressed: () {
+                      homeController.convert();
+                    },
+                    child:
+                        Text('CONVERTER', style: TextStyle(color: Colors.black)))
+              ],
+            ),
           ),
         ),
       ),
