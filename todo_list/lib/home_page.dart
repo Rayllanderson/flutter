9b877876200597f8ccd769275628d471f9efd9f0
Rayllanderson <rayllanderson@gmail.com>
2021-05-26 15:15:11 -0300

    Todo list - Criando os estados da View

--- todo_list/lib/home_page.dart
+++ todo_list/lib/home_page.dart
@@ -1,7 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/controllers/home_controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
+
+  _success() {
+    return ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Item $index'),
+          );
+        });
+  }
+
+  elevatedButtonCentered(String message) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          //tentar novamente
+        },
+        child: Text(message),
+      ),
+    );
+  }
+
+  _loading() {
+    return Center(child: CircularProgressIndicator());
+  }
+
+  _start() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+            child: Text(
+          'Bem vindo',
+          style: TextStyle(fontSize: 25),
+        )),
+        elevatedButtonCentered('Carregar Todo\'s')
+      ],
+    );
+  }
+
+  _stateManagement(HomeState state){
+    switch(state){
+      case HomeState.start:
+        return _start();
+      case HomeState.error:
+        return elevatedButtonCentered('Tentar novamente');
+      case HomeState.success:
+        return _success();
+      case HomeState.loading:
+        return _loading();
+      default:
+        return _start();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,13 +64,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Todo List'),
       ),
-      body: ListView.builder(
-          itemCount: 20,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('Item $index'),
-            );
-          }),
+      body: _stateManagement(HomeState.start),
     );
   }
 }
